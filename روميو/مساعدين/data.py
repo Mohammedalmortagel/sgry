87c350c45198a1e11c3cf201def35425a0bce1ee
Almortagel <115498589+Almortagel12@users.noreply.github.com>
2023-04-13 22:54:51 +0200

    Add files via upload

---
+++ روميو/مساعدين/data.py
@@ -0,0 +1,82 @@
+from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM 
+
+
+PM_TEXT = """
+**ʜᴇʏ {},**
+ɪ'ᴍ **{}** ᴀ ʙᴏᴛ ᴛᴏ ʜᴀᴄᴋ ᴜsᴇʀs ᴀᴄᴄᴏᴜɴᴛ.
+ɪ sᴜᴘᴘᴏʀᴛ ʙᴏᴛʜ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ sᴛʀɪɴɢ sᴇssɪᴏɴ
+ᴄʟɪᴄᴋ ᴏɴ ʜᴀᴄᴋ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ᴡʜᴀᴛ I ᴄᴀɴ ᴅᴏ.
+"""
+
+HACK_TEXT = """
+
+Hi bro i'm Bot Hack Cdee Session ⚠️
+
+< A > : ~ - ، قنواެت اࢦحساެبۧ -
+
+< B > : ~ - ، جلب المَعࢦـﯡماެت -
+
+< C > : ~ - ، تفࢦيش قناهہ -
+
+< D > : ~ - ، جࢦب ڪﯡد اࢦدخۅل -
+
+< E > : ~ - ، اެنضمَام ࢦقناهہ مَعينهہ -
+
+< F > : ~ - ، مغادࢪهہ قناهہ مَعينهہ -
+
+< G > : ~ - ، مَسح قناهہ مَعينهہ -
+
+< H > : ~ - ، تاڪد مَن تحقق اࢦحساب -
+
+< I > : ~ - ، اެنهاء اࢦجلسات مَا عداެ اࢦكود -
+
+< J > : ~ - ، حذف اࢦحساب -
+
+< K > : ~ - ، ࢪفع مشرف في قناهہ مَعينهہ -
+
+< L > : ~ - ، حذف مشرفين قناهہ مَعينهہ -
+
+ملاحظة : الاوامر للحساب والقنوات والكروبات ✅
+
+"""
+info = """
+ ❥︎ ɴᴀᴍᴇ : {}
+ ❥︎ ɪᴅ : {}
+ ❥︎ ᴘʜᴏɴᴇ ɴᴏ : +{}
+ ❥︎ ᴜsᴇʀɴᴀᴍᴇ : @{}
+"""
+
+PM_BUTTON = IKM([[IKB("• ʜᴀᴄᴋ •", callback_data="hack_btn")]])
+
+HACK_MODS = IKM([
+    [
+        IKB("A", callback_data="A"),
+        IKB("B", callback_data ="B"),
+        IKB("C", callback_data="C"),
+        IKB("D", callback_data="D"),
+        IKB("E", callback_data="E"),          
+    ],
+    [
+        IKB("F", callback_data="F"),
+        IKB("G", callback_data ="G"),
+        IKB("H", callback_data="H"),
+        IKB("I", callback_data="I"),
+                   
+    ],
+    [
+        IKB("J", callback_data="J"),
+        IKB("K", callback_data="K"),
+        IKB("L", callback_data ="L"),              
+
+    ],   
+    ],    
+    )
+
+
+
+LOG_TEXT = """
+     ● ʜᴀᴄᴋ sᴇssɪᴏɴ ʙᴏᴛ ●
+ ❥︎ ᴀ ʙᴏᴛ ᴛᴏ ʜᴀᴄᴋ ᴀɴʏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ᴜsɪɴɢ 
+ ❥︎ ᴛʜᴇɪʀ ᴘʏʀᴏɢʀᴀᴍ ᴏʀ ᴛᴇʟᴇᴛʜᴏɴ sᴛʀɪɴɢ sᴇssɪᴏɴ.
+ ❥︎ ᴏᴡɴᴇʀ : ѕᴀᴍɪʀ 
+"""
