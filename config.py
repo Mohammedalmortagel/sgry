ad260e3333c30ef18660b691ef25f54e9b2bcded
Almortagel <115498589+Almortagel12@users.noreply.github.com>
2023-04-13 23:04:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "19031598")) 
-API_HASH = getenv("API_HASH", "14e2d2af6e6dc769e60991cab81027d5") 
-BOT_TOKEN = getenv("BOT_TOKEN", "5499976017:AAHnP-FDpDvxZaoRRMtKd4G9x_i7JaGFUbM")
+API_ID = int(getenv("API_ID", "14911221")) 
+API_HASH = getenv("API_HASH", "a5e14021456afd496e7377331e2e5bcf") 
+BOT_TOKEN = getenv("BOT_TOKEN", "5977145501:AAFpGxJQkgKGOIJP-CvSeR5Q25a-FdTZAPI")
 START_PIC = getenv("START_PIC", "https://telegra.ph//file/87fa102aa588b71f43b08.jpg")
