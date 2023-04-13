8e0e5a8e887b03033d534e38b22021a27155573c
Almortagel <115498589+Almortagel12@users.noreply.github.com>
2023-04-13 22:52:57 +0200

    Add files via upload

---
+++ روميو/__init__.py
@@ -0,0 +1,42 @@
+import os
+import asyncio
+import logging
+from config import *
+from pyrogram import Client
+from rich.console import Console
+from rich.table import Table
+from Romeo.Helpers.data import LOG_TEXT
+from pyromod import listen 
+
+
+if not START_PIC:
+    START_PIC = "https://telegra.ph//file/a9a90059ae47fa840b0b3.jpg"
+
+#rich
+LOG = Console()
+
+#logger
+logging.basicConfig(level=logging.INFO)
+
+#client
+app = Client(
+    "romeorj",
+    api_id = API_ID,
+    api_hash = API_HASH,
+    bot_token = BOT_TOKEN )
+    
+
+
+async def Romeo():
+    os.system("clear")
+    header = Table(show_header=True, header_style="bold green")
+    header.add_column(LOG_TEXT)
+    LOG.print(header)
+    LOG.print(f"[bold cyan]𝐂𝐑𝐄𝐀𝐓𝐎𝐑 ❥︎𝕾🅡︎🅐︎🅙︎𝕾...")
+    LOG.print("[bold yellow]sᴛᴀʀᴛɪɴɢ ʏᴏᴜʀ ʙᴏᴛ ɴᴏᴡ.......")
+    await app.start()    
+    
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(Romeo())
