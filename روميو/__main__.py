7756a841093e44ea5984e3c498454efd8c4b9064
Almortagel <115498589+Almortagel12@users.noreply.github.com>
2023-04-13 22:52:19 +0200

    Create __main__.py

---
+++ روميو/__main__.py
@@ -0,0 +1,21 @@
+
+
+import asyncio
+import importlib
+
+from pyrogram import idle
+from Romeo import LOG
+from Romeo.modules import ALL_MODULES
+
+
+async def start_bot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("Romeo.modules." + all_module)
+    LOG.print("[bold yellow]💞 ʙᴏᴛ sᴛᴀʀᴛᴇᴅ")
+    await idle() 
+    LOG.print("[bold red]ᴄᴀɴᴄᴇʟɪɴɢ ᴀʟʟ ᴛᴀsᴋs.")
+
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(start_bot())
